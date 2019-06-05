230652619e579cb6a6e284871a86a3cb8bb38761
Jeffrey Li <jeffrey@vectorwyse.com>
2019-06-05 11:30:42 -0400

    allow customizable element attributes

--- src/views/dropdown.blade.php
+++ src/views/dropdown.blade.php
@@ -1,19 +1,45 @@
-<dropdown-trigger class="h-9 flex items-center" slot-scope="{toggle}" :handle-click="toggle">
-    <span class="text-90">
-        {{ Config::get('app.available_locales')[app()->getLocale()] }}
-    </span>
-</dropdown-trigger>
-
-<dropdown-menu slot="menu" width="200" direction="rtl">
-    <ul class="list-reset">
-        @foreach(Config::get('app.available_locales') as $code => $description)
-            @if ($code != app()->getLocale())
-                <li>
-                    <a href="/set-locale?locale={{ $code }}" class="block no-underline text-90 hover:bg-30 p-3">
-                        {{ $description }}
-                    </a>
-                </li>
-            @endif
+<dropdown 
+    @if (!empty($attributes['dropdown'])) 
+        @foreach ($attributes['dropdown'] as $name => $value)
+            {{ $name }}="{{ $value }}"
         @endforeach
-    </ul>
-</dropdown-menu>
+    @endif
+>
+    <dropdown-trigger slot-scope="{toggle}" :handle-click="toggle"
+        @if (!empty($attributes['dropdown-trigger'])) 
+            @foreach ($attributes['dropdown-trigger'] as $name => $value)
+                {{ $name }}="{{ $value }}"
+            @endforeach
+        @endif
+    >
+        <span>
+            {{ Config::get('app.available_locales')[app()->getLocale()] }}
+        </span>
+    </dropdown-trigger>
+
+    <dropdown-menu slot="menu"
+        @if (!empty($attributes['dropdown-menu'])) 
+            @foreach ($attributes['dropdown-menu'] as $name => $value)
+                {{ $name }}="{{ $value }}"
+            @endforeach
+        @endif
+    >
+        <ul class="list-reset">
+            @foreach (Config::get('app.available_locales') as $code => $description)
+                @if ($code != app()->getLocale())
+                    <li>
+                        <a href="/set-locale?locale={{ $code }}"
+                            @if (!empty($attributes['a'])) 
+                                @foreach ($attributes['a'] as $name => $value)
+                                    {{ $name }}="{{ $value }}"
+                                @endforeach
+                            @endif
+                        >
+                            {{ $description }}
+                        </a>
+                    </li>
+                @endif
+            @endforeach
+        </ul>
+    </dropdown-menu>
+</dropdown>
